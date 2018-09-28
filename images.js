112852fa9fed877dd0967a48b4d2f2fb0b0a6494
HOFFMAN <19hoffmani@gmail.com>
2018-09-28 10:12:37 -0500

    working timer

--- images.js
+++ images.js
@@ -1,4 +1,23 @@
+timer = setInterval(swapImage, 500);
+
+function swapImage(){
+    id = document.getElementById("imageBig").alt
+    document.getElementsByTagName("title")[0].innerHTML = id;
+    if (id < 2){
+        id++;
+        document.getElementById("imageBig").src = imageSelect[id];
+        document.getElementById("imageBig").alt = id;
+    }
+    else{
+        id = 0;
+        document.getElementById("imageBig").src = imageSelect[id];
+        document.getElementById("imageBig").alt = id;
+    }
+
+}
 imageSelect = ["tree1.jpg", "tree2.jpg", "tree3.jpg"]
 function changeImage(id){
-    document.getElementById("imageBig").src = imageSelect[id]
+    document.getElementById("imageBig").src = imageSelect[id];
+    document.getElementById("imageBig").alt = id;
 }
+//use data-* attribute instead of alt, figure out how to dispaly an alt in imageBig
