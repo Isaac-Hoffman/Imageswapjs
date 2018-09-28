0c95424d85badee0bd1f621a57c89ce52ad19a18
HOFFMAN <19hoffmani@gmail.com>
2018-09-28 07:56:15 -0500

    time

--- images.js
+++ images.js
@@ -1,5 +1,4 @@
-function displayImg(images){
-    var image = document.getElementById("image");
-    image.src = images.src;
-    image.alt = images.alt;
-}
+document.getElementById("img2").addEventListener("click", function(){
+    //alert("click");
+    document.getElementById("image").innerHTML = document.getElementById("img2").innerHTML
+})
